a40207d94d1076d9f61d98d944971e2138f19ff5
PreetMangat <preetmangat93@outlook.com>
2019-10-19 01:28:11 -0500

    Configured devise for JSON only API

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, :controllers => {sessions: 'sessions', registrations: 'registrations'}  
   
   #Devise config:
   root to: "home#index"
